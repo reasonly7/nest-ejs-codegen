f735fdfd12102a0357cc0944b5fc012369e82083
reasonly8 <reasonly8@sina.com>
2024-11-19 15:50:00 +0800

    daily updates

--- src/codegen/codegen.service.ts
+++ src/codegen/codegen.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import * as ejs from 'ejs';
+import * as path from 'node:path';
+import * as fs from 'node:fs';
 
 @Injectable()
 export class CodegenService {
   codegen() {
-    return ejs.render('<%= people.join(", "); %>', {
-      people: ['Top', 'Lilei', 'Hanmeimei'],
-    });
+    return '123';
   }
 }
